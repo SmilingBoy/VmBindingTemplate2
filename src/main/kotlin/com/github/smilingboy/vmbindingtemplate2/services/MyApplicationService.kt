de23586dcd3b9a85f9d115edd7031e2892c90045
GitHub Action <action@github.com>
2022-07-05 02:43:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/smilingboy/vmbindingtemplate2/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.smilingboy.vmbindingtemplate2.services
+
+import com.github.smilingboy.vmbindingtemplate2.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
